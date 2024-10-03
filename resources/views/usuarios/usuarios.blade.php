cfbbd66e93cc767965346bc8a6c4e7a6fceab1f6
Tokiau <josuepineda2024.14@gmail.com>
2024-10-02 18:24:36 -0600

    vidta de usuarios

--- resources/views/usuarios/usuarios.blade.php
+++ resources/views/usuarios/usuarios.blade.php
@@ -1,41 +1,83 @@
+<head>
+    <style>
+        .centroM{
+            margin: 2% 2%;
+
+        };
+        .rounded{
+            border-radius: 100%
+        }
+    </style>
+</head>
 <body>
     @extends('layout.app')
 
     @section('title','Clientes')
 
     @section('content')
-    <h1>Usuarios</h1>
-    <h5>Lista de Clientes</h5>
+    <h1>Clientes</h1>
+    <h5>Usuarios registrados permitidos y no permitidos</h5>
     <hr>
-    <a class="btn btn-success" href="#">Agregar un nuevo cliente</a>
-    <table class="table table-striped">
-        <thead>
+    <a class="btn btn-success centroM" href="#">Agregar un nuevo cliente</a>
+    <div class="centroM">
+        <table class="table table-striped rounded">
+            <thead>
             <tr>
-                <th>ID</th>
-                <th>Nombre</th>
-                <th>Edad</th>
-                <th>Categoría</th>
-                <th>Acciones</th>
+                <th class="bg-dark text-white" >ID</th>
+                <th class="bg-dark text-white" >Nombre</th>
+                <th class="bg-dark text-white" >Edad</th>
+                <th class="bg-dark text-white" >Categoría</th>
+                <th class="bg-dark text-white" >Acciones</th>
             </tr>
         </thead>
         <tbody>
             
             <tr>
                 <td>2</td>
-                <td>2</td>
-                <td>2</td>
-                <td>2</td>
+                <td>Josue Raul Pineda Santos</td>
+                <td>21</td>
+                <td>Informatica</td>
                 <td>
-                    <button>Prestar</button>
-                    <button>Editar</button>
-                    <button>Eliminar</button>
+                    <button class="btn btn-info text-white">Prestar</button>
+                    <button class="btn" style="background-color: #6a0dad; color: white;">Bloquear</button>
+                    <button class="btn btn-warning text-white">Editar</button>
+                    <button class="btn btn-danger">Eliminar</button>                    
                 </td>
             </tr>
             
 
         </tbody>
     </table>
+    <br>
+    <h2>Usuarios no permitidos</h2>
+    <table class="table table-striped rounded">
+        <thead>
+        <tr>
+            <th class="bg-dark text-white" >ID</th>
+            <th class="bg-dark text-white" >Nombre</th>
+            <th class="bg-dark text-white" >Edad</th>
+            <th class="bg-dark text-white" >Categoría</th>
+            <th class="bg-dark text-white" >Acciones</th>
+        </tr>
+    </thead>
+    <tbody>
+        
+        <tr>
+            <td>2</td>
+            <td>Josue Raul Pineda Santos</td>
+            <td>21</td>
+            <td>Informatica</td>
+            <td>
+                <button class="btn btn-info text-white">quitar</button>                  
+            </td>
+        </tr>
+        
+
+    </tbody>
+</table>
     @endsection
+    </div>
+    
 
     @section('scripts')
     {{-- SweetAlert --}}
